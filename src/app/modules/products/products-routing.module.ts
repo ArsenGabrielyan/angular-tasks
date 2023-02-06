2a64eea4a43593ccca65b9a7c5423c2ee267329c
ArsenGabrielyan <arsentech5002@gmail.com>
2023-02-06 17:54:08 +0400

    Added Products table And Design

--- src/app/modules/products/products-routing.module.ts
+++ src/app/modules/products/products-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ProductsComponent } from './products.component';
 
-const routes: Routes = [{path: "", component: ProductsComponent}];
+const routes: Routes = [
+  {path: "", component: ProductsComponent},
+  {path: ":action", component: ProductFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -3,14 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    ProductFormComponent,
   ],
   imports: [
     CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     ProductsRoutingModule
   ]
 })
