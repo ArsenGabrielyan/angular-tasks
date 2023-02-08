07ae71302f62edfd81756fd6ffdc02832cbdbcd3
ArsenGabrielyan <arsentech5002@gmail.com>
2023-02-08 18:33:14 +0400

    Made Search Pipe Pure

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'search'
+  name: 'search',
+  pure: true
 })
 export class SearchPipe implements PipeTransform {
   transform(val: any, term: string){
